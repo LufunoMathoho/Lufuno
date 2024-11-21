17244a34f9467d5cef164521effff63b6c1969c1
LufunoMathoho <163723819+LufunoMathoho@users.noreply.github.com>
2024-11-21 07:45:50 +0000

    Pending changes exported from your codespace

--- app.py
+++ app.py
@@ -1,7 +1,26 @@
-from flask import Flask, render_template 
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime 
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URL']= 'sqlite:///members.db'
+
+db = SQLAlchemy(app)
+
+class members(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+name= db.Column(db String(200), nullable=False)
+data_created=db.Column(db.DateTime, default=)
+
+def __repr__(self):
+    return.'<Name.%r'
+
+members=[]
 
 @app.route("/")
-def index():
-    return "Hello, World"
+def members():
+    title="About members"
+    names=["Lufuno, Tondani, Thanyani"]
+    return render_template("members.html", title=title, names=names)
+
+    
